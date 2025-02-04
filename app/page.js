3c4c28f0795e37d689157a2baa085f5a6270850e
MutableTuple <mutabletuple39@gmail.com>
2025-02-04 11:37:27 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Homepage from "./_components/Homepage";
 import { getAllTheories } from "./_lib/data-service";
 
+export const revalidate = 30;
+
 export default async function Home() {
   const theories = await getAllTheories();
   return <Homepage theories={theories} />;
